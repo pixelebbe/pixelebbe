04574d3f6e753a97985f4d36597c56ad93831927
luap42 <pstrobach15+luap42@gmail.com>
2025-04-30 00:28:10 +0200

    Add event table and lookup

--- database.py
+++ database.py
@@ -2,3 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Event(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(65))
+    slug = db.Column(db.String(15))
+    active = db.Column(db.Boolean())
+
+    @classmethod
+    def from_slug(cls, slug):
+        return cls.query.filter_by(slug=slug).one_or_404()
--- event_view.py
+++ event_view.py
@@ -1,7 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, g
+from database import Event
 
 event_view = Blueprint('event', __name__)
 
+def check_and_apply_event(func):
+    def inner_func(event, *args, **kwargs):
+        event = Event.from_slug(event)
+
+        g.event = event
+
+        return func(*args, **kwargs)
+
+    inner_func.__name__ = func.__name__
+    return inner_func
+
 @event_view.route("/")
-def index(event):
+@check_and_apply_event
+def index():
     return f"welcome to event {event}"
