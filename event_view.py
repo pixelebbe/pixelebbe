59e26d05dda760b90a50bc32d60c7795ec8a61d5
luap42 <pstrobach15+luap42@gmail.com>
2025-05-02 00:40:30 +0200

    Cache images

--- event_view.py
+++ event_view.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, g, render_template, send_file
 from database import db, Event
-
-from PIL import Image
-from io import BytesIO
+import image_helper as imh
 
 event_view = Blueprint('event', __name__)
 
@@ -31,20 +29,4 @@ def beamer():
 @event_view.route("/view.png")
 @check_and_apply_event
 def view_png():
-    dim = g.event.inner_pixel_dimensions
-    imw = (g.event.pixel_width) * dim
-    imh = (g.event.pixel_height) * dim
-    im = Image.new("RGB", [imw, imh], 255)
-    
-    pixels = g.event.pixels.all()
-
-    for pixel in pixels:
-        x = pixel.pos_x * dim + pixel.dim_x
-        y = pixel.pos_y * dim + pixel.dim_y
-        im.putpixel((x, y), pixel.color.getRGB())
-
-    img_io = BytesIO()
-    im.save(img_io, 'PNG')
-    img_io.seek(0)
-
-    return send_file(img_io, mimetype='image/png')
+    return imh.make_or_load_image(g.event)
---
+++ image_helper.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import os
+from flask import send_file
+
+def make_image(event):
+    dim = event.inner_pixel_dimensions
+    imw = (event.pixel_width) * dim
+    imh = (event.pixel_height) * dim
+    im = Image.new("RGB", [imw, imh], 255)
+    
+    pixels = event.pixels.all()
+
+    for pixel in pixels:
+        x = pixel.pos_x * dim + pixel.dim_x
+        y = pixel.pos_y * dim + pixel.dim_y
+        im.putpixel((x, y), pixel.color.getRGB())
+
+    im.save(f'temp/event-{event.slug}.png', 'PNG')
+
+def make_or_load_image(event):
+    if not os.path.exists(f'temp/event-{event.slug}.png'):
+        make_image(event)
+    
+    return send_file(f'temp/event-{event.slug}.png', mimetype='image/png')
