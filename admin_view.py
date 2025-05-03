ab0bde51655998ef7c7038b790063e8fa809aec4
luap42 <pstrobach15+luap42@gmail.com>
2025-05-03 02:45:02 +0200

    Implement setting pixels

--- admin_view.py
+++ admin_view.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, request
 from flask_security import auth_required
-from database import db, Event
+from database import db, Event, Color, Pixel
+from image_helper import make_image
 
 admin_view = Blueprint('admin', __name__)
 
@@ -8,6 +9,33 @@ admin_view = Blueprint('admin', __name__)
 @auth_required()
 def index():
     return render_template("admin/index.html")
+
+
+@admin_view.route("/events")
+@auth_required()
+def events():
+    all_events = Event.query.all()
+    return render_template("admin/events/index.html", all_events=all_events)
+
+
+@admin_view.route("/events/<event>/setpixel", methods=['GET', 'POST'])
+@auth_required()
+def event_set_pixel(event):
+    event = Event.from_slug(event)
+    all_colors = Color.query.all()
+
+    if request.method == 'POST':
+        pos_x = int(request.form['pos_x'])
+        pos_y = int(request.form['pos_y'])
+        color = int(request.form['color'])
+
+        color = Color.query.filter_by(id=color).one()
+        event.pixels.filter_by(pos_x=pos_x, pos_y=pos_y).update({"color_id": color.id})
+        db.session.commit()
+
+        make_image(event)
+
+    return render_template("admin/events/setpixel.html", event=event, all_colors=all_colors)
 
 
 @admin_view.route("/test-event")
