78da3e75909dea078b7da5fe55886a175b3b1a1e
luap42 <pstrobach15+luap42@gmail.com>
2025-05-04 22:17:36 +0200

    Introduce graceful refresh
    WHY DOES THIS WORKETH WHYYYY

---
+++ static/graceful-refresh.js
@@ -0,0 +1,10 @@
+const image = document.querySelector('.beamer img');
+const imageUrl = image.src;
+
+let refreshImage = () => {
+    image.src = imageUrl + '?d=' + Date.now()
+}
+
+window.addEventListener("load", () => {
+    window.setInterval(refreshImage, 2000)
+})
